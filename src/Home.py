e9fb05713c62274f787f06db29a9c9cf1719e577
oriname <agbioriname@gmail.com>
2023-10-25 12:36:57 +0100

    first commit

---
+++ src/Home.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+
+#Config
+st.set_page_config(layout="wide", page_icon="💬", page_title="ChainBotAI 🧑‍💻")
+
+
+
+
+
+#Title
+st.markdown(
+    """
+    <h2 style='text-align: center;'>ChainBotAI, your data-aware assistant 🧑‍💻</h1>
+    """,
+    unsafe_allow_html=True,)
+
+st.markdown("---")
+
+
+#Description
+st.markdown(
+    """ 
+    <h5 style='text-align:center;'>I'm ChainBotAI, an intelligent chatbot. I use large language models to provide
+    context-sensitive interactions. My goal is to help you better understand your data.
+    </h5>
+    """,
+    unsafe_allow_html=True)
+st.markdown("---")
+
+
+
+
+
+
+
+
+
+
---
+++ src/modules/chatbot.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts.prompt import PromptTemplate
+from langchain.callbacks import get_openai_callback
+
+#fix Error: module 'langchain' has no attribute 'verbose'
+import langchain
+langchain.verbose = False
+
+class Chatbot:
+
+    def __init__(self, model_name, temperature, vectors):
+        self.model_name = model_name
+        self.temperature = temperature
+        self.vectors = vectors
+
+    qa_template = """
+        You are ChainBot, a specialized AI assistant designed to answer questions related to inventory management and supply chain management based on the provided dataset. Please adhere to the following guidelines:
+    
+        - Your primary source of information is the dataset represented in the context.
+        - If the question is based on the dataset, provide a detailed and accurate answer.
+        - If you don't have enough information in the dataset to answer, politely state that you don't know.
+        - Do NOT make up answers or provide information outside the context.
+        - If the question is not related to inventory management, supply chain management, or the context, politely inform the user that you are designed to specifically answer questions within those domains and based on the provided dataset.
+
+
+        context: {context}
+        =========
+        question: {question}
+        ======
+        """
+
+    QA_PROMPT = PromptTemplate(template=qa_template, input_variables=["context","question" ])
+
+    def conversational_chat(self, query):
+        """
+        Start a conversational chat with a model via Langchain
+        """
+        llm = ChatOpenAI(model_name=self.model_name, temperature=self.temperature)
+
+        retriever = self.vectors.as_retriever()
+
+
+        chain = ConversationalRetrievalChain.from_llm(llm=llm,
+            retriever=retriever, verbose=True, return_source_documents=True, max_tokens_limit=16384, combine_docs_chain_kwargs={'prompt': self.QA_PROMPT})
+
+        chain_input = {"question": query, "chat_history": st.session_state["history"]}
+        result = chain(chain_input)
+
+        st.session_state["history"].append((query, result["answer"]))
+        #count_tokens_chain(chain, chain_input)
+        return result["answer"]
+
+
+def count_tokens_chain(chain, query):
+    with get_openai_callback() as cb:
+        result = chain.run(query)
+        st.write(f'###### Tokens used in this conversation : {cb.total_tokens} tokens')
+    return result 
+
+    
+    
